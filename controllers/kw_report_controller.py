5001d7cc4ee59089f32feacb481f6d8a9bcfb656
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-22 17:05:42 +0530

    feat: add report one controller

--- controllers/kw_report_controller.py
+++ controllers/kw_report_controller.py
@@ -6,3 +6,10 @@ class ReportController(http.Controller):
     @http.route('/report', type='http', auth='public', website=True)
     def render_report_page(self):
         return http.request.render('kw_assignment.report_page', {})
+
+    @http.route('/report/one', type='http', auth='public', website=True)
+    def render_report_one_page(self):
+        subjects = http.request.env['kw_subject'].sudo().search([])
+        return http.request.render('kw_assignment.report_one_page', {
+            'subjects': subjects
+        })
