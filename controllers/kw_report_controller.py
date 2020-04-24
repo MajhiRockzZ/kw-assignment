1b349c9a1dd5c6356798e6c87f511fabdd6e54e4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-24 18:35:16 +0530

    feat: employee name subject highest mark complete

--- controllers/kw_report_controller.py
+++ controllers/kw_report_controller.py
@@ -42,6 +42,7 @@ class ReportController(http.Controller):
 
         # ==== [ SUBJECT WISE HIGHEST MARK ] ====
         subjects = http.request.env['kw_subject'].sudo().search([])
+        employees = http.request.env['hr.employee'].sudo().search([])
 
         unique_subject_list = []
 
@@ -53,15 +54,26 @@ class ReportController(http.Controller):
                 unique_subject_list.append(subject.subject_name)
 
         for subject_name in unique_subject_list:
-            subject_data = http.request.env['kw_subject'].sudo().search([('subject_name', '=', subject_name)])
+            subject_data = http.request.env['kw_subject'].sudo().search(
+                [('subject_name', '=', subject_name)])
+
             max_mark = 0
+            employee_college_id = None
+            employee_name = None
+
             for data in subject_data:
                 if data.subject_mark > max_mark:
                     max_mark = data.subject_mark
+                    employee_college_id = data.subject_semester_id.semester_stream_id.stream_college_id.id
+
+            for employee in employees:
+                if employee_college_id == employee.emp_college_id.id:
+                    employee_name = employee.name
+
             subject_max_mark['subject_list'].append({
+                'employee_name': employee_name,
                 'subject_name': subject_name,
                 'max_mark': max_mark
             })
 
         return http.request.render('kw_assignment.report_two_page', subject_max_mark)
-
